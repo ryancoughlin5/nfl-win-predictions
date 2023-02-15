e0544e0544f52afd6454ea1f67545f25e1356464
ryancoughlin5 <ryancoughlin5@gmail.com>
2023-02-14 20:31:50 -0500

    added no huddle column

--- script.R
+++ script.R
@@ -12,4 +12,4 @@ column_names <- tibble(colnames(pbp))
 cols_keep <- c('play_id', 'game_id', 'old_game_id', 'home_team', 'away_team', 'season_type', 'week', 'posteam', 
                'posteam_type', 'defteam', 'side_of_field', 'yardline_100', 'game_date', 'quarter_seconds_remaining', 
                'half_seconds_remaining', 'game_seconds_remaining', 'game_half', 'quarter_end', 'drive', 'sp', 'qtr',
-               'down', 'goal_to_go', 'time', 'yrdln', 'ydstogo', 'ydsnet', 'play_type', 'yards_gained', 'shotgun')
+               'down', 'goal_to_go', 'time', 'yrdln', 'ydstogo', 'ydsnet', 'play_type', 'yards_gained', 'shotgun', 'no_huddle')
