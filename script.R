ffeb25e7b9cb2411fc13a54562c4ff1199f4dfb4
ryancoughlin5 <ryancoughlin5@gmail.com>
2023-02-21 17:02:40 -0500

    updated script wtih some new charts

--- script.R
+++ script.R
@@ -1,5 +1,6 @@
 library(nflverse)
 library(tidyverse)
+library(plotly)
 pbp <- nflfastR::load_pbp(2010:2022)
 
 column_names <- tibble(colnames(pbp))
@@ -16,44 +17,46 @@ cols_keep <- c('play_id', 'game_id', 'old_game_id', 'home_team', 'away_team', 's
 
 pbp <- pbp[cols_keep]
 go_for_it <- pbp %>% filter(down == '4' & (play_type %in% c('pass', 'run')))
-all_fourth <- pbp %>%  filter(down == '4')
+all_fourth <- pbp %>%  filter(down == '4', qtr %in% c(1, 2, 3, 4))
 
 mean(go_for_it$ydstogo)
 median(go_for_it$ydstogo)
 
 # percent of time teams go for it on 4th down
-count(go_for_it[[]])/count(all_fourth)*100
+#count(go_for_it[[]])/count(all_fourth)*100
 
 
 #### Get total times a coach has gone for it ####
 home_coach_go <- go_for_it %>%
   rename('coach' = 'home_coach') %>% 
   group_by(coach) %>% 
-  filter(posteam_type == 'home') %>% 
-  count(coach, name = 'count_home')
+  filter(posteam_type == 'home') %>%
+  summarise(count_home = n(), conversions = sum(fourth_down_converted))
 
 away_coach_go <- go_for_it %>%
   rename('coach' = 'away_coach') %>% 
   group_by(coach) %>% 
-  filter(posteam_type == 'away') %>% 
-  count(coach, name = 'count_away')
+  filter(posteam_type == 'away') %>%
+  summarise(count_away = n(), conversions = sum(fourth_down_converted)) 
 
 go_for_it_coach <- merge(away_coach_go, home_coach_go, by = 'coach')
 go_for_it_coach$go = go_for_it_coach$count_home + go_for_it_coach$count_away
-go_for_it_coach <- go_for_it_coach[, c(1, 4)]
+go_for_it_coach$converted = go_for_it_coach$conversions.x + go_for_it_coach$conversions.y
+go_for_it_coach <- go_for_it_coach[, c(1, 6, 7)]
 
 #### All 4th downs in a coaches career ####
 home_coach_4 <- all_fourth %>%
   rename('coach' = 'home_coach') %>% 
   group_by(coach) %>% 
-  filter(posteam_type == 'home') %>% 
-  count(coach, name = 'count_home')
+  filter(posteam_type == 'home') %>%
+  summarise(count_home = n()) 
+  
 
 away_coach_4 <- all_fourth %>%
   rename('coach' = 'away_coach') %>% 
   group_by(coach) %>% 
   filter(posteam_type == 'away') %>% 
-  count(coach, name = 'count_away')
+  summarise(count_away = n()) 
 
 all_fourth_coach <- merge(away_coach_4, home_coach_4, by = 'coach')
 all_fourth_coach$all = all_fourth_coach$count_home + all_fourth_coach$count_away
@@ -62,5 +65,22 @@ all_fourth_coach <- all_fourth_coach[, c(1, 4)]
 #### Merge go_for_it_coach and all_fourth_coach to get 4th down go percentage ####
 go_rates <- merge(go_for_it_coach, all_fourth_coach, by='coach')
 go_rates$go_percentage <- round((go_rates$go/go_rates$all) * 100, 2)
+go_rates$conversion_rate <- round((go_rates$converted/go_rates$go) * 100, 2)
 go_rates <- (go_rates[order(go_rates$go_percentage, decreasing = T), ]) 
+go_rates <- go_rates[go_rates$all > 100, ]
 go_rates <- mutate(go_rates, rank = row_number())
+
+#### Plotting coach fourth down go percentages (min 100 4th downs) ####
+plot_ly(x = go_rates$coach, y = go_rates$go_percentage, type="bar") %>% 
+  layout(xaxis = list(categoryorder = "total descending"))
+
+#### Plotting coach fourth down conversion artes (min 100 4th downs) ####
+plot_ly(x = go_rates$coach, y = go_rates$conversion_rate, type="bar") %>% 
+  layout(xaxis = list(categoryorder = "total descending"))
+     
+#### Fourth downs gone for by quarter ###
+all_fourth %>% 
+  mutate(Quarter = as.character(qtr)) %>% 
+  group_by(Quarter) %>% 
+  summarise(count = n()) %>% 
+  plot_ly(x = ~Quarter, y=~count, type="bar")
